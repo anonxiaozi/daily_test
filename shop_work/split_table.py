df6b1a1cfb73c70a196adb2c6004d3c3876c2fef
anonxiaozi <bloke_anon@126.com>
2019-04-08 12:26:55 +0800

    添加季度、半年拆分，添加命令行参数

--- shop_work/split_table.py
+++ shop_work/split_table.py
@@ -4,10 +4,9 @@
 
 import pymongo
 import datetime
-import random
-import time
-import bson.binary
 import sys
+import calendar
+import argparse
 
 
 class SplitDB(object):
@@ -66,9 +65,9 @@ class SplitDB(object):
         #     authMechanism=self.args["SCRAM-SHA-256"]
         # )
         self.db = self.conn[self.args["db"]]
-        self.write_table = self.db[self.args["write_table"]]
-        self.read_table = self.db[self.args["read_table"]]
-        self.filter_rule.append({"$out": self.args["write_table"]})
+        self.write_table = self.db[self.args["write"]]
+        self.read_table = self.db[self.args["read"]]
+        self.filter_rule.append({"$out": self.args["write"]})
 
     def insert_many(self, data):
         try:
@@ -78,7 +77,9 @@ class SplitDB(object):
 
     def get_data(self):
         getattr(self, "do_{}".format(self.args["filter"]), self.echo)()
-        if self.args["filter"] == "all":
+        if self.args["filter"] == "all":        # 拷贝collection全部内容
+            cursor = self.read_table.aggregate(self.filter_rule)
+            cursor.close()
             return
         if not isinstance(self.filter_rule, list):
             sys.exit(1)
@@ -114,37 +115,85 @@ class SplitDB(object):
         }
 
     def do_season(self):
-        # s = datetime.datetime.strptime("{year}-{month}-{day}".format(**self.args), "%Y-%m-%d")
-        # self.filter_rule[1]["$match"] = {
-        #     'date': {
-        #         '$gte': s,
-        #         '$lt': e
-        #     }
-        # }
+        start_date = datetime.datetime.strptime("{year}-{month}-{day}".format(**self.args), "%Y-%m-%d")
+        current_month_days = calendar.monthrange(start_date.year, start_date.month)[1]
+        current_month_remain_days = current_month_days - start_date.day
+        next_month_1 = start_date + datetime.timedelta(days = current_month_remain_days) + datetime.timedelta(days = self.get_next_month_days(start_date))
+        next_month_2 = next_month_1 + datetime.timedelta(days = self.get_next_month_days(next_month_1))
+        end_season_date = next_month_2  + datetime.timedelta(days = start_date.day)
+        self.filter_rule[1]["$match"] = {
+            'date': {
+                '$gte': start_date,
+                '$lt': end_season_date
+            }
+        }
+        return end_season_date
+
+    def do_halfYear(self):
+        start_date = datetime.datetime.strptime("{year}-{month}-{day}".format(**self.args), "%Y-%m-%d")
+        end_season_date_1 = self.do_season()
+        current_month_days = calendar.monthrange(end_season_date_1.year, end_season_date_1.month)[1]
+        current_month_remain_days = current_month_days - end_season_date_1.day
+        next_month_1 = end_season_date_1 + datetime.timedelta(days=current_month_remain_days) + datetime.timedelta(days=self.get_next_month_days(end_season_date_1))
+        next_month_2 = next_month_1 + datetime.timedelta(days=self.get_next_month_days(next_month_1))
+        harf_year_date = next_month_2 + datetime.timedelta(days=start_date.day)
+        self.filter_rule[1]["$match"] = {
+            'date': {
+                '$gte': start_date,
+                '$lt': harf_year_date
+            }
+        }
 
     def do_all(self):
-        self.filter_rule = self.filter_rule.append({"$out": self.args["write_table"]})
+        self.filter_rule = self.filter_rule.append({"$out": self.args["write"]})
 
     def run(self):
-        return getattr(self, self.args["operate"], self.echo)()
+        return getattr(self, "get_data", self.echo)()
 
     @staticmethod
     def echo():
         print("Illega operation")
 
+    @staticmethod
+    def get_next_month_days(before_month_date):
+        try:
+            next_month_days = calendar.monthrange(before_month_date.year, before_month_date.month + 1)
+        except calendar.IllegalMonthError:
+            next_month_days = calendar.monthrange(before_month_date.year + 1, 1)
+        return next_month_days[1]
+
+
+def get_args():
+    """
+    命令行参数
+    """
+    arg = argparse.ArgumentParser(prog="Split_collection", usage='%(prog)s filter [options]')
+    arg.add_argument("filter", type=str, choices=["year", "month", "week", "season", "halfYear"], help="filter name")
+    arg.add_argument("--host", type=str, help="DB host, default=%(default)s", default="10.15.101.63")
+    arg.add_argument("--port", type=int, help="DB port, default=%(default)s", default=27027)
+    arg.add_argument("--db", type=str, help="DB name, default=%(default)s", default="blockchain_test")
+    arg.add_argument("-r", "--read", type=str, help="read collection name")
+    arg.add_argument("-w", "--write", type=str, help="write collection name")
+    arg.add_argument("--year", type=int, help="year", required=True)
+    arg.add_argument("--month", type=int, help="month")
+    arg.add_argument("--day", type=int, help="day")
+    return arg
+
 
 if __name__ == "__main__":
-    args = {
-        "host": "",
-        "port": 27017,
-        "filter": "week",   # 过滤规则
-        "operate": "get_data",
-        "db": "",
-        "read_table": "",
-        "write_table": "test111",
-        "year": 2019,
-        "month": 2,
-        "day": 7
-    }
+    # args = {
+    #     "host": "10.15.101.63",
+    #     "port": 27027,
+    #     "filter": "season",   # 过滤规则
+    #     "operate": "get_data",
+    #     "db": "blockchain_test",
+    #     "read": "StoreTransaction_hash",
+    #     "write": "test111",
+    #     "year": 2018,
+    #     "month": 11,
+    #     "day": 20
+    # }
+
+    args = vars(get_args().parse_args())
     opt = SplitDB(args=args)
     data = opt.run()
