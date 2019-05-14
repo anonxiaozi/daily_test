8842343f8d84a1447d6aa67479fd14c6972c1e72
anonxiaozi <bloke_anon@126.com>
2019-05-14 18:16:55 +0800

    不按时间分组

--- analysis_pre/BrandShopDay.py
+++ analysis_pre/BrandShopDay.py
@@ -32,140 +32,126 @@ class BrandShopDay(ConnectDB):
 
 
 if __name__ == "__main__":
-    filter_lvjian = [{'$match': {
-          'title': { '$regex': '绿箭' }
-        }}, {'$project': {
-          'title': '$title'
-        }}, {'$lookup': {
-          'from': 'ProductTransaction',
-          'let': {
-              'productid': '$_id'
-          },
-          'pipeline': [
-              {
-                  '$match': {
-                      '$expr': {
-                          '$eq': [
-                              '$productInfo', '$$productid'
-                          ]
-                      }
-                  }
-              }, {
-                  '$project': {
-                      'transaction': '$transaction',
-                      'amount': '$amount',
-                      'sum': { '$multiply': ['$price', '$amount'] }
-                  }
-              }
-          ],
-          'as': 'producttransaction'
-        }}, {'$unwind': {
-          'path': '$producttransaction'
-        }}, {'$lookup': {
-          'from': 'Transaction',
-          'let': {
-              'transactionid': '$producttransaction.transaction'
-          },
-          'pipeline': [
-              {
-                  '$match': {
-                      '$expr': {
-                          '$eq': [
-                              '$_id', '$$transactionid'
-                          ]
-                      }
-                  }
-              }, {
-                  '$project': {
-                      '_id': 0,
-                      'paiedTime': '$paiedTime',
-                      'store': '$store'
-                  }
-              }
-          ],
-          'as': 'transaction'
-        }}, {'$unwind': {
-          'path': '$transaction'
-        }}, {'$lookup': {
-          'from': 'StoreTransaction',
-          'let': {
-              'store': '$transaction.store'
-          },
-          'pipeline': [
-              {
-                  '$match': {
-                      '$expr': {
-                          '$eq': [
-                              '$storeId', '$$store'
-                          ]
-                      }
-                  }
-              }, {
-                  '$project': {
-                      '_id': 0,
-                      'fulladdress': '$fulladdress'
-                  }
-              }
-          ],
-          'as': 'storetransaction'
-        }}, {'$unwind': {
-          'path': '$storetransaction'
-        }}, {'$project': {
-          'storeid': '$transaction.store',
-          'title': '$title',
-          'date': {
-            '$dateToParts': {
-              'date': '$transaction.paiedTime'
+    filter_lvjian = [
+        {'$match': {
+            'title': { '$regex': '绿箭' }
+        }},
+        {'$project': {
+            'title': '$title'
+        }},
+        {'$lookup': {
+            'from': 'ProductTransaction',
+            'let': {
+                'productid': '$_id'
+            },
+            'pipeline': [
+                {
+                '$match': {
+                    '$expr': {
+                        '$eq': [
+                            '$productInfo', '$$productid'
+                        ]
+                    }
+                }},
+                {
+                '$project': {
+                    'transaction': '$transaction',
+                    'amount': '$amount',
+                    'sum': { '$multiply': ['$price', '$amount'] }
+                }}
+            ],
+                'as': 'producttransaction'
+            }},
+            {'$unwind': {
+                'path': '$producttransaction'
+            }},
+            {'$lookup': {
+                'from': 'Transaction',
+                'let': {
+                    'transactionid': '$producttransaction.transaction'
+                },
+                'pipeline': [
+                    {
+                        '$match': {
+                            '$expr': {
+                                '$eq': [
+                                    '$_id', '$$transactionid'
+                                ]
+                            }
+                        }
+                    },
+                    {
+                        '$project': {
+                            '_id': 0,
+                            'paiedTime': '$paiedTime',
+                            'store': '$store'
+                        }
+                    }
+                ],
+                'as': 'transaction'
+            }},
+            {'$unwind': {
+                'path': '$transaction'
+            }},
+            {'$lookup': {
+                'from': 'StoreTransaction',
+                'let': {
+                    'store': '$transaction.store'
+                },
+                'pipeline': [
+                    {
+                        '$match': {
+                            '$expr': {
+                                '$eq': [
+                                    '$storeId', '$$store'
+                                ]
+                            }
+                        }
+                    },
+                    {
+                        '$project': {
+                            '_id': 0,
+                            'fulladdress': '$fulladdress'
+                        }
+                    }
+                ],
+                'as': 'storetransaction'
+            }},
+            {'$unwind': {
+                'path': '$storetransaction'
+            }},
+            {'$project': {
+                'storeid': '$transaction.store',
+                'title': '$title',
+                'amount': '$producttransaction.amount',
+                'sum': '$producttransaction.sum',
+                'fulladdress': '$storetransaction.fulladdress'
+            }},
+            {'$group': {
+                '_id': '$storeid',
+                'sum': {'$sum': '$sum'},
+                'amount': {'$sum': '$amount'},
+                'data': {'$push': {
+                    'title': '$title',
+                    'fulladdress': '$fulladdress'
+                }},
+            }},
+            {'$project': {
+                '_id': 0,
+                'storeid': '$_id',
+                'sum': '$sum',
+                'amount': '$amount',
+                'data': {'$arrayElemAt': ['$data', 0]},
+            }},
+            {'$project': {
+                'storeid': '$storeid',
+                'sum': '$sum',
+                'amount': '$amount',
+                'fulladdress': '$data.fulladdress',
+                'title': '$data.title',
+                }
             }
-           },
-          'amount': '$producttransaction.amount',
-          'sum': '$producttransaction.sum',
-          'fulladdress': '$storetransaction.fulladdress'
-        }}, {'$project': {
-          'storeid': '$storeid',
-          'title': '$title',
-          'year': '$date.year',
-          'month': '$date.month',
-          'day': '$date.day',
-          'amount': '$amount',
-          'sum': '$sum',
-          'fulladdress': '$fulladdress'
-        }}, {'$group': {
-          '_id': {
-            'storeid': '$storeid',
-            'year': '$year',
-            'month': '$month',
-            'day': '$day'
-          },
-          'sum': {'$sum': '$sum'},
-          'amount': {'$sum': '$amount'},
-          'data': {'$push': {
-            'title': '$title',
-            'fulladdress': '$fulladdress'
-          }},
-        }}, {'$project': {
-          '_id': 0,
-          'storeid': '$_id.storeid',
-          'sum': '$sum',
-          'amount': '$amount',
-          'data': {'$arrayElemAt': ['$data', 0]},
-          'date': {
-            '$dateFromParts': {
-            'year': '$_id.year',
-            'month': '$_id.month',
-            'day': '$_id.day'
-            }
-          }
-        }}, {'$project': {
-          'storeid': '$storeid',
-          'sum': '$sum',
-          'amount': '$amount',
-          'fulladdress': '$data.fulladdress',
-          'title': '$data.title',
-          'date': '$date'
-          }
-        }
-    ]
+        ]
     args = {
         'host': '10.15.101.252',
         'port': 27017,
